2e0b5c05542d8a7161df1b4e09bb014a682c6a82
Héctor Vallvé <hectorvallveg@gmail.com>
2023-01-13 16:43:31 +0100

    no message

---
+++ app/src/main/java/com/example/projecteorgueapi23/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.projecteorgueapi23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button iniciarPreguntes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iniciarPreguntes = findViewById(R.id.botoInici);
+
+        iniciarPreguntes.setOnClickListener(v -> openPreguntes());
+
+    }
+
+    public void openPreguntes(){
+        Intent intent = new Intent(this, PantallaPreguntes.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/projecteorgueapi23/PantallaPreguntes.java
@@ -0,0 +1,15 @@
+package com.example.projecteorgueapi23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PantallaPreguntes extends AppCompatActivity {
+
+
+
+
+
+
+
+
+
+}
