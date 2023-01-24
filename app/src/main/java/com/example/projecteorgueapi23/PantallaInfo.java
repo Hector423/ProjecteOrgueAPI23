4fbfd4a42e1682ca2729c5112d8aa2e5e97558ff
Héctor Vallvé <hectorvallveg@gmail.com>
2023-01-24 16:54:19 +0100

    acabar pantalla info

---
+++ app/src/main/java/com/example/projecteorgueapi23/PantallaInfo.java
@@ -0,0 +1,14 @@
+package com.example.projecteorgueapi23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_info);
+    }
+}
