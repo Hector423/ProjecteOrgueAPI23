e0f10331253954f5ca0e71a7cbe9269730d7cedb
EloySPAIN <yole.tres@gmail.com>
2023-03-17 16:12:46 +0100

    boton sonido solo en uno

--- app/src/main/java/com/example/projecteorgueapi23/Musica.java
+++ app/src/main/java/com/example/projecteorgueapi23/Musica.java
@@ -3,17 +3,24 @@ package com.example.projecteorgueapi23;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 
 import java.io.IOException;
 import java.util.Random;
 
 public class Musica {
+    // MediaPlayer variables
     private static final int cancion = R.raw.fondo;
-    private static final int musicButton = R.raw.boton;
     private static MediaPlayer mp = null, mpBoton = null;
     private static int audioIndex = 0;
     private static boolean muted = false;
+
+    // SoundPool variables
+    private static SoundPool sp = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+    private static int soundId;
+
 
     static {
         mp = new MediaPlayer();
@@ -22,11 +29,6 @@ public class Musica {
 
     public static MediaPlayer getMp() {
         return mp;
-    }
-
-    public static void musicaBotones(Context context){
-        mpBoton = MediaPlayer.create(context, R.raw.boton);
-        mpBoton.start();
     }
 
     public static void playAudio(Context context) {
@@ -73,5 +75,15 @@ public class Musica {
 
     public static void setMuted(boolean muted) {
         Musica.muted = muted;
+    }
+
+    public static void soundButton(Context context) {
+        soundId = sp.load(context, R.raw.boton, 1);
+        sp.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                sp.play(soundId, 1, 1, 0, 0, 1);
+            }
+        });
     }
 }
