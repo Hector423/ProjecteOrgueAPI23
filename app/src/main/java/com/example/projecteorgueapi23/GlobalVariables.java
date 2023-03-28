41cfc0d87452cccdb76f7190847c0649ad4416a0
Héctor Vallvé <hectorvallveg@gmail.com>
2023-03-28 15:42:37 +0200

    comentaris

--- app/src/main/java/com/example/projecteorgueapi23/GlobalVariables.java
+++ app/src/main/java/com/example/projecteorgueapi23/GlobalVariables.java
@@ -1,6 +1,9 @@
 package com.example.projecteorgueapi23;
 
 public class GlobalVariables {
+    /**
+     * Variables globales
+     */
     public static int cont = 0;
     public static int puntuacion = 0;
     public static int fallos = 0;
